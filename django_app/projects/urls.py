10c03edbd31a04cfea302b3559ff290673994814
Lee HanYeong <dev@azelf.com>
2016-10-20 10:16:30 +0900

    메인페이지 구현

--- django_app/projects/urls.py
+++ django_app/projects/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
+from . import views
 
 urlpatterns = [
+    # url(r'^$', views.index, name='')
     url(r'^member/', include('member.urls', namespace='member')),
     url(r'^blog/', include('projects.blog.urls', namespace='blog')),
     url(r'^video/', include('projects.video.urls', namespace='video')),
